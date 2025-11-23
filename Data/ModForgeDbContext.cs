4d838ac44e472464959cd50e53bb53e399241bea
mattnorthcutt <matt_northcutt@icloud.com>
2025-11-22 19:07:19 -0600

    Initial Setup

---
+++ Data/ModForgeDbContext.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ModForge.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ModForge.Data;
+public class ModForgeDbContext : IdentityDbContext<IdentityUser>
+{
+    private readonly IConfiguration _configuration;
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
+    public ModForgeDbContext(DbContextOptions<ModForgeDbContext> context, IConfiguration config) : base(context)
+    {
+        _configuration = config;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+        {
+            Id = "c3aaeb97-d2ba-4a53-a521-4eea61e59b35",
+            Name = "Admin",
+            NormalizedName = "admin"
+        });
+
+        modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
+        {
+            Id = "dbc40bc6-0829-4ac5-a3ed-180f5e916a5f",
+            UserName = "Administrator",
+            Email = "admina@strator.comx",
+            PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, _configuration["AdminPassword"])
+        });
+
+        modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+        {
+            RoleId = "c3aaeb97-d2ba-4a53-a521-4eea61e59b35",
+            UserId = "dbc40bc6-0829-4ac5-a3ed-180f5e916a5f"
+        });
+        modelBuilder.Entity<UserProfile>().HasData(new UserProfile
+        {
+            Id = 1,
+            IdentityUserId = "dbc40bc6-0829-4ac5-a3ed-180f5e916a5f",
+            FirstName = "Admina",
+            LastName = "Strator",
+            ImageLocation = "https://robohash.org/numquamutut.png?size=150x150&set=set1",
+            CreateDateTime = new DateTime(2022, 1, 25)
+        });
+
+    }  
+}
---
+++ Models/DTOs/UserProfileDTO.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace ModForge.Models.DTOs;
+public class UserProfileDTO
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [NotMapped]
+    public string UserName { get; set; }
+
+    [NotMapped]
+    public string Email { get; set; }
+
+    public DateTime CreateDateTime { get; set; }
+
+    [DataType(DataType.Url)]
+    [MaxLength(255)]
+    public string ImageLocation { get; set; }
+}
